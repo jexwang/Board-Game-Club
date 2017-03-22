fb3c4127f338ada9c8fc4dfbe32c598e8f4e3ce0
王冠綸 <ToriWang@wangguanlunde-MacBook-Pro.local>
2017-03-22 21:12:24 +0800

    專案開始，匯入Firebase/Auth/Database，登入與註冊介面完成

---
+++ BoardGameClub/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BoardGameClub
+//
+//  Created by 王冠綸 on 2017/3/22.
+//  Copyright © 2017年 jexwang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
